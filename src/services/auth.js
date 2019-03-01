60e7ff54735fe80d69c693fd3a2bdad130b11dd6
Anshuman <anshu5074@gmail.com>
2019-03-01 21:59:40 +0530

    add auth service

---
+++ src/services/auth.js
@@ -0,0 +1,28 @@
+import jwt from 'jsonwebtoken';
+import { UserInputError, ForbiddenError } from 'apollo-server';
+import constants from '../config/constants';
+import User from '../models/User';
+
+export async function requireAuth(user) {
+  if (!user || !user._id) {
+    throw new ForbiddenError('Unauthorized!');
+  }
+
+  const me = await User.findById(user._id);
+
+  if (!me) {
+    throw new ForbiddenError('Unauthorized!');
+  }
+
+  return me;
+}
+
+export function decodeToken(token) {
+  const arr = token.split(' ');
+
+  if (arr[0] === 'meow') {
+    return jwt.verify(arr[1], constants.JWT_SECRET);
+  }
+
+  throw new UserInputError('Token not valid!');
+}
