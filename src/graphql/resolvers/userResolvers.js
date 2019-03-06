9d91861e6f77054e333cce64208694b373abee83
Anshuman <anshu5074@gmail.com>
2019-03-06 22:00:38 +0530

    add stats for user

--- src/graphql/resolvers/userResolvers.js
+++ src/graphql/resolvers/userResolvers.js
@@ -1,6 +1,8 @@
 import { AuthenticationError, ValidationError } from 'apollo-server';
 
 import User from '../../models/User';
+import Project from '../../models/Project';
+import Issue from '../../models/Issue';
 import { requireAuth } from '../../services/auth';
 
 export default {
@@ -35,7 +37,31 @@ export default {
       return new AuthenticationError("couldn't recognize you!");
     }
   },
-
+  stats: async (_, args, { user }) => {
+    try {
+      const Projects = await Project.find({ user: user._id });
+      const issueStats = { unAssigned: 0, fixed: 0, assigned: 0 };
+      if (Projects.length) {
+        Projects.forEach(async (item) => {
+          const issues = await Issue.find({ project: item._id });
+          if (issues) {
+            for (let i = 0; i < issues.length; i++) {
+              if (issues[i].status === 'Un-assigned') {
+                issueStats.unAssigned += 1;
+              } else if (issues[i].status === 'Fixed') {
+                issueStats.fixed += 1;
+              } else {
+                issueStats.assigned += 1;
+              }
+            }
+          }
+        });
+      }
+      return { projects: Projects.length || 0, ...issueStats };
+    } catch (error) {
+      return new AuthenticationError("couldn't recognize you!");
+    }
+  },
   updateMe: async (_, args, { user }) => {
     try {
       const me = await requireAuth(user);
