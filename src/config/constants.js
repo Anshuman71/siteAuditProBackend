ba4dee6846dae7eb4985c36c509a3996eafce144
Anshuman <anshu5074@gmail.com>
2019-03-11 17:26:22 +0530

    edit constants

--- src/config/constants.js
+++ src/config/constants.js
@@ -1,6 +1,6 @@
 export default {
   PORT: process.env.PORT || 3000,
-  JWT_SECRET: '@meow@',
+  JWT_SECRET: process.env.JWT_SECRET || '@your-secret-goes-here@',
   DB_URL: process.env.MONGO_DB_URI || 'mongodb://localhost/siteAuditpro-dev',
   GRAPHQL_PATH: '/graphql',
 };
