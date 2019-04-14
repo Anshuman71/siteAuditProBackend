edc0e8acc2e124f155df0655126c4a247ec80463
Ashu96 <bhardwajashu96@hotmail.com>
2019-04-14 15:10:02 +0530

    Fix schema for `Project`

--- src/graphql/schema.js
+++ src/graphql/schema.js
@@ -34,7 +34,7 @@ export default `
   type Issue {
     _id: ID!
     title: String!
-    project: String!
+    projectId: String!
     description: String
     assignedTo: String
     status: String
@@ -55,8 +55,8 @@ export default `
     title: String!
     client: String!
     auditorCompany: String!
-    auditorName: String
     location: String!
+    auditorName: String!
     user: User
     createdAt: Date!
     updatedAt: Date!
@@ -66,16 +66,16 @@ export default `
     getIssueById(_id: ID!): Issue
     getIssuesByProjectId(_id:ID!): [Issue]
     getProjectsByUser: [Project]
-    getProjectById(_id:ID): [Project]
+    getProjectById(_id:ID): Project
     stats: Stats
     me: Me
   }
 
   type Mutation {
-    createIssue(title: String!, project: String!, description:String, assignedTo:String, status:String,imageSrc: [String]): Issue
+    createIssue(title: String!, projectId: String!, description:String, assignedTo:String, status:String,imageSrc: [String]): Issue
     deleteIssue(_id: ID!): Status
     updateIssue(_id: ID!,title: String, description:String, assignedTo:String, status:String,imageSrc: [String]): Issue
-    createProject(title: String!, client: String!, auditorCompany:String, auditorName:String, location:String): Project
+    createProject(title: String!, client: String!, auditorCompany: String!, auditorName: String!, location: String!): Project
     deleteProject(_id: ID!): Status
     updateProject(_id: ID!,title: String, client: String, auditorCompany:String, auditorName:String, location:String): Project
     login(email: String!, userName: String!, FCMToken: String, gender: String!, avatar: String): Auth
